5d515f33b495a48e393137fd934d852ee22693b6
João Oliveira <j.oliveiradg@gmail.com>
2023-02-04 13:29:42 -0300

    [DIA 27] first commit, Create Project

---
+++ android/app/src/main/kotlin/br/com/jboliveira/ubbi/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.jboliveira.ubbi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:ubbi/telas/Home.dart';
+
+void main() => runApp(MaterialApp(
+  title: "Ubbi",
+  home: Home(),
+  debugShowCheckedModeBanner: false,
+));
